a61ba032dd78c8b6444bab229aa982fe1968acb6
Seongjin <seongjinkim1123@gmail.com>
2020-01-28 23:59:55 +0900

    20200128

--- JavaScript/main.js
+++ JavaScript/main.js
@@ -1,12 +1,17 @@
 //#1 -- For loop in Javascript.
 const nemo = ['nemo'];
+const everone = ['dory', 'bruce', 'marlin', 'nemo', 'gill', 'bloat', 'nigel', 'squirt', 'darla', 'hank'];
+const large = new Array(100).fill('nemo');
 
 function findNemo1(array) {
+  let t0 = performance.now();
   for (let i = 0; i < array.length; i++) {
     if (array[i] === 'nemo') {
       console.log('Found NEMO!');
     }
   }
+  let t1 = performance.now();
+  console.log('Call to find Nemo took : ' + (t1 - t0) + ' milliseconds');
 }
 
-findNemo1(nemo);
+findNemo1(large);
